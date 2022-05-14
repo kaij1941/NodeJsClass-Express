97b8f9bc0897257db9af64af701dabdf1493d8a1
曹凱嵐Jacky <kaij1941@gmail.com>
2022-05-15 02:37:41 +0800

    feat　增加Sample 動態路由 + url給參數 limit

--- app.js
+++ app.js
@@ -11,10 +11,18 @@ app.get('/',function(req,res){
 app.get('/user',function(req,res){
     res.send('網址/user的路由')
 })
+
 //動態路由
 app.get('/user/:name/:Code',function(req,res){
     res.send('網址/user的路由,且傳參數name為"'+req.params.name+'",Code為"'+req.params.Code+'"')
     //傳遞結果(當路由為/user/TestName/00123)=>   網址/user的路由,且傳參數name為"TestName",Code為"00123"
+})
+
+//動態路由 + url給參數 limit
+app.get('/user/:name',function(req,res){
+    var limit = req.query.limit
+    res.send('網址/user的路由,且傳參數name為"'+req.params.name+'",limit為"'+ limit+'"')
+    //傳遞結果(當路由為/user/TestName?limit=10)=>   網址/user的路由,且傳參數name為"TestName",limit為"10"
 })
 
 //監聽 port
