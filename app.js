aa7fb27e9770641fc19fb0f53f8331530ac19b37
曹凱嵐Jacky <kaij1941@gmail.com>
2022-05-14 16:06:54 +0800

    進階路由 及 動態路由範例

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.get('/',function(req,res){
 })
 
 
+app.get('/user',function(req,res){
+    res.send('網址/user的路由')
+})
+//動態路由
+app.get('/user/:name/:Code',function(req,res){
+    res.send('網址/user的路由,且傳參數name為"'+req.params.name+'",Code為"'+req.params.Code+'"')
+    //傳遞結果(當路由為/user/TestName/00123)=>   網址/user的路由,且傳參數name為"TestName",Code為"00123"
+})
 
 //監聽 port
 var port = process.env.PORT || 3005;
